0f136d454519355b1ab640fd52d5e5fcbda3fe6c
bakir <bsabljica1@etf.unsa.ba>
2018-11-30 17:53:42 +0100

    napravljen interfejs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    public GridPane glavniProzor;
+    public GridPane leftupperProzor;
+    public GridPane rightupperProzor;
+    public GridPane rightlowerProzor;
+    public GridPane leftlowerProzor;
+
+    public void prijavaClick(ActionEvent actionEvent) {
+        System.out.println("Prijava uspjesna");
+    }
 }
