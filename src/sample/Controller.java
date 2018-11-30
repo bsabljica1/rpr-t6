c82dc3dc3dec5421bc3afbbbfc6fde75d810aa2d
bakir <bsabljica1@etf.unsa.ba>
2018-11-30 21:37:52 +0100

    izgled formulara

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,9 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class Controller {
@@ -9,6 +12,18 @@ public class Controller {
     public GridPane rightupperProzor;
     public GridPane rightlowerProzor;
     public GridPane leftlowerProzor;
+    public TextField imePolje;
+    public TextField prezimePolje;
+    public TextField indexPolje;
+    public TextField jmbgPolje;
+    public TextField datumrodjPolje;
+    public ComboBox mjestorodjPolje;
+    public TextField kontaktadresaPolje;
+    public TextField kontakttelefonPolje;
+    public TextField emailPolje;
+    public ChoiceBox odsjekPolje;
+    public ChoiceBox godinastudijaPolje;
+    public ChoiceBox ciklusPolje;
 
     public void prijavaClick(ActionEvent actionEvent) {
         System.out.println("Prijava uspjesna");
